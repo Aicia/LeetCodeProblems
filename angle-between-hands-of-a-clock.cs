206a1a7b6c8dceaeb5e65953e2f818aa5ad7e114
Aicia <62385410+Aicia@users.noreply.github.com>
2020-05-27 20:02:39 -0400

    file formatting fix

--- angle-between-hands-of-a-clock.cs
+++ angle-between-hands-of-a-clock.cs
@@ -3,22 +3,27 @@
 public class Solution {
     public double AngleClock(int hour, int minutes) {
         
-         
         double minangle = 6 * minutes;
-        double hrangle = 30 * hour + .5 * minutes;
-        
-        
+        double hrangle = 30 * hour + .5 * minutes;   
         double angle = 0;
         
         if(hrangle <= minangle)
+        {
             angle = minangle - hrangle;
+        }
         else
+        {
             angle = hrangle - minangle;
+        }
         
         if (360 - angle < angle)
+        {
             return 360-angle;
+        }
         else
+        {
             return angle;
+        }
 
     }
 }
